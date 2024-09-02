9309d2502ce1ce9a1bd8ac90537dc3c2f64217e2
Excelsius314 <marekdaniv@googlemail.com>
2024-09-02 13:36:37 +0200

    Debug

--- blob_detection/cv2_blob_detector_node.py
+++ blob_detection/cv2_blob_detector_node.py
@@ -1,11 +1,11 @@
 import cv2 as cv
-from cv_bridge import CvBridge
+from cv_bridge import CvBridge, CvBridgeError
 import numpy as np
 
 import rclpy
 from rclpy.node import Node
 
-from sensor_msgs.msg import Image
+from sensor_msgs.msg import Image, CompressedImage
 from std_msgs.msg import Int16
 import matplotlib.pyplot as plt
 
@@ -15,7 +15,6 @@ class BlobDetector(Node):
 
     def __init__(self):
         super().__init__('cv2_blob_detector')
-        self.img_subscriber = self.create_subscription(Image, "/image_raw", self.detect_blobs, 1)
         self.bridge = CvBridge()
 
         self.detection_publisher = self.create_publisher(Image, "/blob_detections", 10)
@@ -24,12 +23,21 @@ class BlobDetector(Node):
 
         self.color_angles = {"red" : (0, 15), "green": (65, 80), "blue": (100, 150)}
         self.colors = ["red"] #, "blue", "green"]
+        self.color_to_robot_name = {"red":"epuck1", "blue":"epuck2", "green":"epuck3"}
 
-        self.prev_known_centers = {color: (0,0) for color in self.colors}
+        self.prev_known_centers = {color: None for color in self.colors}
         self.color_debuggers = {color: self.create_publisher(Image, "/color_debug/{}".format(color), 10) for color in self.colors}
-        self.color_center_pubs = {color: self.create_publisher(Int16, "/x_position/{}".format(color), 10) for color in self.colors}
 
-        self.blob_count_treshhold =10
+
+        self.image_subs = [self.create_subscription(CompressedImage, "/{}/image_raw/compressed".format(self.color_to_robot_name[color]),
+                                                    lambda msg: self.detect_blobs(msg, color), 1) for color in self.colors]
+
+
+        self.color_center_pubs = {color: self.create_publisher(Int16, "/{}/ball_coordinates".format(self.color_to_robot_name[color]), 10) for color in self.colors}
+        self.draw_colors = {"red": (255, 0, 0), "blue": (0, 0, 255), "green": (0, 255, 0)}
+
+
+        self.blob_count_treshhold = 10
         self.blob_bin_size = 10
 
         self.min_saturation = 120
@@ -94,9 +102,9 @@ class BlobDetector(Node):
             valid_points_h = valid_points[0][in_largest_blob]
             valid_points_w = valid_points[1][in_largest_blob]
 
-
+            print("got largest blob:")
             combined_valids = np.vstack((valid_points_h, valid_points_w)).transpose().astype(float)
-
+            print(combined_valids)
             if len(combined_valids) == 0:
                 return None
 
@@ -167,76 +175,70 @@ class BlobDetector(Node):
             res = least_squares(f, np.array([m_h.copy(), m_w.copy(), r.copy()]))
 
 
-            return res.x[:2]
+            return np.array(res.x[:2])
 
 
 
 
-    def detect_blobs(self, img_msg):
+    def detect_blobs(self, img_msg, color):
         
-        cv_image = self.bridge.imgmsg_to_cv2(img_msg, desired_encoding="passthrough")
+        #cv_image = self.bridge.imgmsg_to_cv2(img_msg, desired_encoding="passthrough")
+
+        try:
+             if type(img_msg) is CompressedImage:
+                 #np_arr = np.fromstring(img_msg.data, np.uint8)
+                 #cv_image = cv.imdecode(np_arr, cv.IMREAD_COLOR)  # decode
+                 cv_image = self.bridge.compressed_imgmsg_to_cv2(img_msg, desired_encoding="rgb8")
+             else:
+                 cv_image = self.bridge.imgmsg_to_cv2(img_msg, desired_encoding="rgb8")
+
+        except CvBridgeError as e:
+            print(e)
 
         h,w = cv_image.shape[0], cv_image.shape[1]
-        #cv_image[:,:, 1] = (0.95* cv_image[:,:, 1]).astype("uint8")
 
         # hsv
         hsv_im = cv.cvtColor(cv_image, cv.COLOR_RGB2HSV)
         is_not_a_color = hsv_im[:, : , 1] < self.min_saturation
 
-        print(hsv_im[is_not_a_color])
+        color_range = self.color_angles[color]
+        is_not_correct_color = np.logical_or(hsv_im[:, : , 0] > color_range[1], hsv_im[:, : , 0] < color_range[0])
+        non_ball = np.logical_or(is_not_a_color, is_not_correct_color)
 
-        centers = []
+        single_color_img =  hsv_im.copy()
+        single_color_img[non_ball] *= 0
+        debug_msg = self.bridge.cv2_to_imgmsg(cv.cvtColor(single_color_img, cv.COLOR_HSV2RGB), encoding='rgb8')
+        debug_msg.header = img_msg.header
+        self.color_debuggers[color].publish(debug_msg)
 
-        for color in self.colors:
+        valid_points = np.logical_not(non_ball).nonzero()
+        center = self.calculate_blob_center(valid_points, h, w)
 
-            color_range = self.color_angles[color]
-
-            is_not_correct_color = np.logical_or(hsv_im[:, : , 0] > color_range[1], hsv_im[:, : , 0] < color_range[0])
-            non_ball = np.logical_or(is_not_a_color, is_not_correct_color)
-
-            single_color_img =  hsv_im.copy()
-            single_color_img[non_ball] *= 0
-            debug_msg = self.bridge.cv2_to_imgmsg(cv.cvtColor(single_color_img, cv.COLOR_HSV2RGB), encoding='rgb8')
-            debug_msg.header = img_msg.header
-            self.color_debuggers[color].publish(debug_msg)
-
-            valid_points = np.logical_not(non_ball).nonzero()
-            centers.append(self.calculate_blob_center(valid_points, h, w))
-
-        draw_colors = {"red": (255, 0, 0), "blue": (0, 0, 255), "green": (0, 255, 0)}
-        for center, color in zip(centers, self.colors):
-
-            if center is None:
+        if center is None:
+            if self.prev_known_centers[color] is not None:
                 center = self.prev_known_centers[color]
             else:
-                self.prev_known_centers[color] = center
-            cv_image = cv.circle(cv_image, (int(center[1]), int(center[0])), 2, draw_colors[color], 1)
+                center = np.array([0, 0])
+        else:
+            #if self.prev_known_centers[color] is not None:
+            #    center = center/4 + 3*self.prev_known_centers[color]/4
+            self.prev_known_centers[color] = center
+
+            cv_image = cv.circle(cv_image, (int(center[1]), int(center[0])), 2, self.draw_colors[color], 1)
 
             coord_msg = Int16()
             coord_msg.data = int(center[1] - w/2)
             self.color_center_pubs[color].publish(coord_msg)
 
-            
-        #plt.title("Only largest blob points")
-        #plt.scatter(border_points[:, 1], h-border_points[:, 0], color="red")
-        #print("Results: {}".format(res.x))
-        #plt.scatter([res.x[1]], [h - res.x[0]], color="blue")
-        #plt.scatter([m_w], [h - m_h], color="green")
-
-        # Show keypoints
-        #cv.imshow("Keypoints", im_with_keypoints)
-        # plt.show()
-        #cv.waitKey(0)
         print("Publish results")
-        #cv_image = cv.cvtColor(cv_image, cv.COLOR_BGR2RGB)
         result_msg = self.bridge.cv2_to_imgmsg(cv_image, encoding='rgb8')
         result_msg.header = img_msg.header
         self.detection_publisher.publish(result_msg)
 
 
-def main():
+def main(args=None):
 
-    rclpy.init()
+    rclpy.init(args=args)
     blob_detector = BlobDetector()
     rclpy.spin(blob_detector)
     blob_detector.destroy_node()
